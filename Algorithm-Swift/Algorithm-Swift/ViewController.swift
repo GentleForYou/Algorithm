e90d2d0a6969a269ff51c3afb87f93da6774648d
shaowenle <shaowenle@paypalm.cn>
2018-11-08 17:06:14 +0800

    修改文件夹

---
+++ Algorithm-Swift/Algorithm-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Algorithm-Swift
+//
+//  Created by 掌上汇通Mac on 2018/11/8.
+//  Copyright © 2018 掌上汇通Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Algorithm-Swift/Algorithm-SwiftTests/Algorithm_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  Algorithm_SwiftTests.swift
+//  Algorithm-SwiftTests
+//
+//  Created by 掌上汇通Mac on 2018/11/8.
+//  Copyright © 2018 掌上汇通Mac. All rights reserved.
+//
+
+import XCTest
+@testable import Algorithm_Swift
+
+class Algorithm_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
